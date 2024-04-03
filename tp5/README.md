b2ac827254f628dd815c106fb769d1c561c73344
Daniel Gago <up202108791@edu.fe.up.pt>
2024-04-03 10:01:00 +0100

    Ex1 from tp5 done

--- tp5/README.md
+++ tp5/README.md
@@ -1,1 +1,13 @@
-# [COPY FROM PREVIOUS TP AND ADJUST]
+# CG 2023/2024
+
+## Group T11G05
+
+## TP 5 Notes
+
+- In exercise 1 we had alot of difficulty understanding shaders in general and how to work with variables across the vertex and the shader files but we managed to do it.
+
+![Figure 1: Blue Yellow Bule](screenshots/cg-t11g05-tp5-1.png)
+Figure 1: Blue Yellow Bule
+
+![Figure 2: My Sepia](screenshots/cg-t11g05-tp5-2.png)
+Figure 2: My Sepia
--- tp5/ShaderScene.js
+++ tp5/ShaderScene.js
@@ -89,7 +89,8 @@ export class ShaderScene extends CGFscene {
 			new CGFshader(this.gl, "shaders/texture3anim.vert", "shaders/texture3anim.frag"),
 			new CGFshader(this.gl, "shaders/texture1.vert", "shaders/sepia.frag"),
 			new CGFshader(this.gl, "shaders/texture1.vert", "shaders/convolution.frag"),
-			new CGFshader(this.gl, "shaders/yb.vert", "shaders/yb.frag")
+			new CGFshader(this.gl, "shaders/yb.vert", "shaders/yb.frag"),
+			new CGFshader(this.gl, "shaders/texture1.vert", "shaders/mysepia.frag")
 		];
 
 		// additional texture will have to be bound to texture unit 1 later, when using the shader, with "this.texture2.bind(1);"
@@ -111,7 +112,8 @@ export class ShaderScene extends CGFscene {
 			'Animation example': 6,
 			'Sepia': 7,
 			'Convolution': 8,
-			'Yellow Blue': 9
+			'Yellow Blue': 9,
+			'My Sepia': 10
 		};
 
 		// shader code panels references
@@ -192,11 +194,13 @@ export class ShaderScene extends CGFscene {
 	// called periodically (as per setUpdatePeriod() in init())
 	update(t) {
 		// only shader 6 is using time factor
-		if (this.selectedExampleShader == 6)
+		if (this.selectedExampleShader == 6 || this.selectedExampleShader == 9 || this.selectedExampleShader == 10)
 			// Dividing the time by 100 "slows down" the variation (i.e. in 100 ms timeFactor increases 1 unit).
 			// Doing the modulus (%) by 100 makes the timeFactor loop between 0 and 99
 			// ( so the loop period of timeFactor is 100 times 100 ms = 10s ; the actual animation loop depends on how timeFactor is used in the shader )
 			this.testShaders[6].setUniformsValues({ timeFactor: t / 100 % 100 });
+			this.testShaders[9].setUniformsValues({ timeFactor: t / 100 % 100 });
+			this.testShaders[10].setUniformsValues({ timeFactor: t / 100 % 100 });
 	}
 
 	// main display function
